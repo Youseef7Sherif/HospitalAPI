43b81455be6e14f18ce2ee5510ab16c8eb12140f
Youseef7Sherif <youseefsheref7@gmail.com>
2025-12-18 07:33:33 +0200

    update database connection

--- obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/HospitalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee3c3483e750e9d7feccfaa698599bb68269d98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9867c404f5cb44c213e3260cbfaadc03ecd37c38")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
